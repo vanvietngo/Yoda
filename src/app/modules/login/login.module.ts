b850fd0559b352a5ab98f725dec2182bfb7a608b
viet.ngovan <viet.ngovan@halliburton.com>
2023-07-19 18:05:55 +0700

    page login

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+
 @NgModule({
   declarations: [LoginComponent],
   imports: [
